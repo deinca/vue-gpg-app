bb490eb793fe3cc8f462cdedd483344afded2aac
deinca <deincawh@gmail.com>
2023-07-08 21:37:37 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,27 @@
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
-import GamesList from './components/GamesList.vue'
+import GamesList from "./components/GamesList.vue";
 // import Game from './types/game'
-
 
 export default defineComponent({
   name: "App",
   components: { GamesList },
   data() {
     return {
-      games: []
+      games: [],
     };
   },
   mounted() {
-    fetch(
-      "https://api.rawg.io/api/games?key=c73433c137764cfcb5fc789d1d008a79"
-    )
+    fetch("https://api.rawg.io/api/games?key=c73433c137764cfcb5fc789d1d008a79")
       .then(function (response) {
         return response.json();
       })
-      .then(data => {
-        this.games = data.results
-        console.log(data.results)
-        });
-  }
+      .then((data) => {
+        this.games = data.results;
+        console.log(data.results);
+      });
+  },
 });
 </script>
 
